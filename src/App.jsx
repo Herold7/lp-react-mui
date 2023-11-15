449e1ee04b5dfe413d9dff18341373ec7d1a6a07
Jensone <hello@jensone.com>
2023-11-15 09:29:24 +0100

    Add new dependencies and update UI components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Box, Stack, Typography, Fab } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Typography variant="subtitle1" textTransform={"uppercase"}>
+        Caractéristiques
+      </Typography>
+      <Typography variant="h3" fontWeight={600}>
+        Plus de puissance pour plus d'efficacité
+      </Typography>
+      <Stack direction="row" spacing={2} paddingTop={5}>
+        <Box sx={{ width: '50%', height: 450, borderRadius: '2vw 4vw', bgcolor: "info.main", position: 'relative'}}>
+        
+          <Fab color="info.light" aria-label="add" sx={{ position: 'absolute', bottom: '5%', right: '5%', }}>
+            <AddIcon />
+          </Fab>
+        </Box>
+        <Stack width="50%" spacing={2}>
+          <Box sx={{ width: '100%', height: '50%', borderRadius: '2vw', bgcolor: "secondary.main", position: 'relative'}}>
+            
+            <Fab color="info.light" aria-label="add" sx={{ position: 'absolute', bottom: '5%', right: '5%', }}>
+              <AddIcon />
+            </Fab>
+          </Box>
+          <Box sx={{ width: '100%', height: '50%', borderRadius: '2vw', bgcolor: "success.main", position: 'relative'}}>
+            
+            <Fab color="info.light" aria-label="add" sx={{ position: 'absolute', bottom: '5%', right: '5%', }}>
+              <AddIcon />
+            </Fab>
+          </Box>
+        </Stack>
+      </Stack>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
